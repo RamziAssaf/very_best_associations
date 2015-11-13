4c19a58b009f0137bef6c3f3d6a38cd023118592
RamziAssaf <rassaf@chicagobooth.edu>
2015-11-13 15:49:30 -0600

    In class

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -1,2 +1,6 @@
 class Neighborhood < ActiveRecord::Base
+	validates :name, :presence => true
+	validates :name, :uniqueness => { :city => :address }
+
+	has_many :venues
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,8 @@
 class Venue < ActiveRecord::Base
+	validates :name, :uniqueness => { :scope => :address }
+	validates :name, :presence => true
+	validates :neihborhood_id, :presence => true
+
+	belongs_to :neighborhood
+	has_many :favorites
 end
